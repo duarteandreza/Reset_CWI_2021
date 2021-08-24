284fd296dd0cf9f1a157c8a103cbaaf52b03187c
Andreza Araújo Duarte <88801231+duarteandreza@users.noreply.github.com>
2021-08-24 10:32:43 -0300

    Create aula03_passandoargumentos.java

---
+++ Módulo 2/aula03_passandoargumentos.java
@@ -0,0 +1,10 @@
+/* Exercício:
+Crie um arquivo chamado AlunoResetComParametro.java
+Implemente um programa que imprima na tela o texto "Sou aluno do Level 1 do CWI Reset! Me chamo {SEU NOME}"
+Compile e execute o programa */
+
+class AlunoResetComParametro{
+    public static void main (String [] args){
+        System.out.println ("Sou aluna do Level 1 do CWI Reset! Me chamo " + args[0]);
+    }
+}
