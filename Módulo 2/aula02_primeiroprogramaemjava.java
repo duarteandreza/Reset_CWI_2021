8839b6d5d532bdc87c1d890646b19974246713f1
Andreza Araújo Duarte <88801231+duarteandreza@users.noreply.github.com>
2021-08-24 10:01:14 -0300

    Create aula02_primeiroprogramaemjava.java

---
+++ Módulo 2/aula02_primeiroprogramaemjava.java
@@ -0,0 +1,10 @@
+/*Exercício:
+Crie um arquivo chamado AlunoReset.java
+Implemente um programa que imprima na tela o texto "Sou aluno do Level 1 do CWI Reset!"
+Compile e execute o programa*/
+
+class AlunoReset {
+    public static void main (String [] args){
+        System.out.println ("Sou aluno do Level 1 do CWI Reset!");
+    }
+}
